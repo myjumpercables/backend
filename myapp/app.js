18b56f69687341c77cb18a0baccd2247651d64fe
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-18 14:49:37 -0500

    Trying to add cors again

--- myapp/app.js
+++ myapp/app.js
@@ -25,6 +25,19 @@ app.use(function(req, res) {
   console.log(req.body);
   res.send('OK');
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
