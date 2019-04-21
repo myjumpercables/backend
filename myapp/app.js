fbda6484b26d6b07c9b2529b1ec98c407470d802
Sarah Kaiser <sjkaiser@smu.edu>
2019-04-21 13:05:32 -0500

    app.js

--- myapp/app.js
+++ myapp/app.js
@@ -19,17 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function(req, res) {
   console.log(req.headers);
   console.log(req.cookies);
   console.log(req.body);
   res.send('OK');
 });
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.get('/', function(req, res, next) {
   // Handle the get for this route
