df19c37e3f4fa5038ac5f9fe4351984c7515098f
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-25 11:22:37 -0500

    Adding pulling from mysql database

--- myapp/routes/create-account.js
+++ myapp/routes/create-account.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
-// var connection = mysql.createConnection({
-//   host: '35.232.83.9',
-//   user: 'root',
-//   password: 'dblab123',
-//   database: 'maintTracker'
-// })
+var connection = mysql.createConnection({
+  host: '35.232.83.9',
+  user: 'root',
+  password: 'dblab123',
+  database: 'maintTracker'
+})
 
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  //connection.connect()
+  connection.connect()
+  //connection.query('INSERT INTO user (username, password) VALUES (req.body.username,)')
   res.send('Create account stuff');
 });
 
--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -2,22 +2,27 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer();
-// var mysql = require('mysql')
-// var connection = mysql.createConnection({
-//   host: '35.232.83.9',
-//   user: 'root',
-//   password: 'dblab123',
-//   database: 'maintTracker'
-// })
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: '35.232.83.9',
+  user: 'root',
+  password: 'dblab123',
+  database: 'maintTracker'
+})
 
 
 /* GET listing. */
 router.post('/', upload.none(), function(req, res, next) {
-  //console.log(req.body);
+  console.log(req.body);
   //console.log(req.params.id);
-  //connection.connect()
-  //connection.query('SELECT user_id FROM users IF ')
-  res.send('Login Stuff');
+  connection.connect()
+  connection.query('SELECT user_id FROM users', (err,rows){
+    if(err) throw err;
+    console.log('Data received form Db:\n');
+    console.log(rows)
+
+  })
+  res.send(rows);
 });
 
 module.exports = router;
