305c91877056ba779ce77c49aa86fb2c2ea6e616
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-26 17:14:17 -0500

    Same as last

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -21,7 +21,7 @@ router.post('/', upload.array(), function(req, res, next) {
   //console.log(req.params.id);
   connection.query(`SELECT user_id, type, username FROM user WHERE username = '${req.body.username}' AND password = '${req.body.password}';`,(err,rows)=> {
     if(err) throw err;
-    if (rows) === []
+    if (rows === [])
       next(createError(401));
     console.log('Data received form Db:\n');
     console.log(rows);
