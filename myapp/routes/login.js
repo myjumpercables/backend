209a32f6a7dc388a5e02001eb0806437f2ac1be5
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-26 17:33:25 -0500

    Same as last

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -22,7 +22,7 @@ router.post('/', upload.array(), function(req, res, next) {
   connection.query(`SELECT user_id, type, username FROM user WHERE username = '${req.body.username}' AND password = '${req.body.password}';`,(err,rows)=> {
     if(err) throw err;
     if (rows.length === 0) {
-        res.status(401);
+        next(createError(401));
         console.log('ERROR');
         }
     else{
