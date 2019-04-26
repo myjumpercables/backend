b6d59c9f35994ee651e2916269e1e926e55e2a61
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-26 16:11:42 -0500

    Same as last

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var router = express.Router();
-// var multer = require('multer');
-// var upload = multer();
-// var mysql = require('mysql')
-// var connection = mysql.createConnection({
-//   host: '35.232.83.9',
-//   user: 'root',
-//   password: 'dblab123',
-//   database: 'maintTracker'
-// })
-// connection.connect()
+var multer = require('multer');
+var upload = multer();
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: '35.232.83.9',
+  user: 'root',
+  password: 'dblab123',
+  database: 'maintTracker'
+})
+connection.connect()
 
 /* GET listing. */
 //put upload.none() before function for multer
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
-  //console.log(req.params.id);
-//   connection.query('SELECT * FROM users',(err,rows)=> {
-//     if(err) throw err;
-//     console.log('Data received form Db:\n');
-//     console.log(rows);
-//
-// });
+  console.log(req.params.id);
+  connection.query('SELECT * FROM users',(err,rows)=> {
+    if(err) throw err;
+    console.log('Data received form Db:\n');
+    console.log(rows);
+
+});
 res.send(rows);
 });
 
