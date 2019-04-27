b0d60c439fd63ee7156dbadc6d5893206448a0e4
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-26 19:52:23 -0500

    Still testing login

--- myapp/database.js
+++ myapp/database.js
@@ -9,7 +9,7 @@ class Database {
         });
         this.connection.connect();
     }
-    query( sql, args ) {
+    async query( sql, args ) {
         return new Promise( ( resolve, reject ) => {
             this.connection.query( sql, args, ( err, rows ) => {
                 if ( err )
