3cbb27898f2be2dd1ecaee4d3966c5200c170179
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-26 20:09:19 -0500

    Still testing login

--- myapp/routes/create-account.js
+++ myapp/routes/create-account.js
@@ -25,7 +25,7 @@ router.post('/', upload.array(), function(req, res, next) {
       database.query(`INSERT INTO user (username,password) VALUES ('${req.body.username}', '${req.body.password}');`)
     }
     console.log(rows);
-  }) .finally({database.close()});
+  })
   // connection.query(`SELECT username FROM user WHERE username = '${req.body.username}';`,(err,rows)=>{
   //   if(err) throw err;
   //   // if(rows.length != 0){
