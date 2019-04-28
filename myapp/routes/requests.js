fe027b0f65d1d1d81c65a5409e4254b7eb1ee524
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-28 17:37:42 -0500

    Fixing route errors

--- myapp/routes/requests.js
+++ myapp/routes/requests.js
@@ -15,7 +15,7 @@ var database = new Database();
 //In REQUESTS ROUTER
 router.post('/add/:user_id', upload.array(), function(req, res, next){
   database.query(
-    `INSERT INTO requests_table (user_id, state, company_id)
+    `INSERT INTO request_table (user_id, state, company_id)
     VALUES ('${req.params.user_id}', 'NULL', '${req.body.company_id}');`
   )
   .then(res.send("OK"))
@@ -27,7 +27,7 @@ router.post('/add/:user_id', upload.array(), function(req, res, next){
 
 router.get('/getRequests/:user_id', upload.array(), function(req, res, next){
   database.query(
-    `SELECT companyId, username FROM requests_table JOIN user
+    `SELECT companyId, username FROM request_table JOIN user
      ON request.company_id = user.user_id
      WHERE request.user_id = ${req.params.user_id} AND state = IS NULL;`
   )
@@ -40,7 +40,7 @@ router.get('/getRequests/:user_id', upload.array(), function(req, res, next){
 
 router.post('/update/:user_id', upload.array(), function(req, res, next){
   database.query(
-    `UPDATE requests_table
+    `UPDATE request_table
      SET state = true
      WHERE user_id = ${req.params.user_id}
      AND company_id = ${req.body.company_id}
@@ -55,7 +55,7 @@ router.post('/update/:user_id', upload.array(), function(req, res, next){
 
 router.post('/delete/:user_id', upload.array(), function(req, res, next){
   database.query(
-    `DELETE FROM requests_table
+    `DELETE FROM request_table
      WHERE user_id = ${req.params.user_id}
      AND company_id = ${req.body.company_id}
      `
