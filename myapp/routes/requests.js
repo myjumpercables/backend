17768bbe1f99a9f2803eef3f02b10c3cc5be084d
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-28 16:01:40 -0500

    Added requests route, repair route, services route, and cars route

---
+++ myapp/routes/requests.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+var Database = require('../database.js')
+var multer = require('multer');
+var upload = multer();
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: '35.232.83.9',
+//   user: 'root',
+//   password: 'dblab123',
+//   database: 'maintTracker'
+// })
+var database = new Database();
+
+//In REQUESTS ROUTER
+router.post('/add/:userId/', upload.array(), function(req, res, next){
+  datebase.query(
+    `INSERT INTO request (userId, state, companyId)
+    VALUES ('${req.params.userId}', 'NULL', '${req.body.companyId}');`
+  )
+  .then(res.send("OK"))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).send();
+  })
+})
+
+router.get('/getRequests/:userId/', upload.array(), function(req, res, next){
+  database.query(
+    `SELECT companyId, username FROM request JOIN user
+     ON request.companyId = user.userId
+     WHERE request.userId = ${req.params.userId} AND state = IS NULL;`
+  )
+  .then(res.send("OK"))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).send();
+  })
+})
+
+router.post('/update/:userId/', upload.array(), function(req, res, next){
+  database.query(
+    `UPDATE requests
+     SET state = true
+     WHERE userId = ${req.params.userId}
+     AND companyId = ${req.body.companyId}
+     `
+  )
+  .then(res.send("OK"))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).send();
+  })
+})
+
+router.post('/delete/:userId/', upload.array(), function(req, res, next){
+  database.query(
+    `DELETE FROM requests
+     WHERE userId = ${req.params.userId}
+     AND companyId = ${req.body.companyId}
+     `
+  )
+  .then(res.send("OK"))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).send();
+  })
+});
+
+
+module.exports = router;
---
+++ myapp/routes/services.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var Database = require('../database.js')
+var multer = require('multer');
+var upload = multer();
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: '35.232.83.9',
+//   user: 'root',
+//   password: 'dblab123',
+//   database: 'maintTracker'
+// })
+var database = new Database();
+
+//In SERVICES ROUTER
+router.post('/add/:id', upload.array(), function(req, res, next){
+  datebase.query(
+    `INSERT INTO services(subject, text, date, car_id)
+    VALUES ('${req.body.subject}', '${req.body.text}', '${req.body.date}', '${req.params.id}');`
+  ).then(res.send("OK"))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).send();
+  })
+});
+
+module.exports = router;
