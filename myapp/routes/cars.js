a4f471a33ebeb68fde66bca1a56265d2ac7d1106
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-04-28 17:15:55 -0500

    Fixing route errors

--- myapp/routes/cars.js
+++ myapp/routes/cars.js
@@ -1,19 +1,58 @@
-
 var express = require('express');
 var router = express.Router();
-// var mysql = require('mysql')
-// var connection = mysql.createConnection({
-//   host: '35.232.83.9',
-//   user: 'root',
-//   password: 'dblab123',
-//   database: 'maintTracker'
-// })
-
-
+var Database = require('../database.js')
+var multer = require('multer');
+var upload = multer();
+var database = new Database();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', upload.array(), function(req, res, next) {
   //connection.connect()
-  res.send('Car Information');
+  //get all cars
+  database.query(
+    `SELECT car_id, make, model, year from cars_table WHERE user_id = ${req.body.id};`
+  ).then((rows, err) => {
+    if (err) throw err;
+    return rows.map((carRow) => {
+      //add services arrray to carRow
+      carRow['services'] = database.query(
+        `SELECT service_id, subject, text, date FROM repair_table WHERE car_id = '${carRow.car_id}';`
+      ).then((serviceRows, err) => {
+        if (err) throw err;
+        return serviceRows.map((serviceRow)=>{
+          serviceRow['repairs'] = database.query(
+            `SELECT repair_id, cost, date FROM repair_table WHERE service_id = '${serviceRow.service_id}';`
+          ).then((repairRows, err) => {
+            if (err) throw err;
+            return repairRows;
+          })
+          return serviceRow;
+        })
+      })
+      // map carRow to corresponding row element
+      return carRow;
+    })
+  }).then((fullRowResult) =>{
+    res.send(fullRowResult);
+  }).catch((err) =>{
+    console.log(err);
+    res.status("500").send("crash");
+  })
 });
+//ADD CAR
+//add car
+router.post('/add/:id', upload.array(), function(req, res,next){
+  console.log(body);
+  database.query(
+    `INSERT INTO cars_table (make, model, year, user_id)
+  VALUES ('${req.body.make}', '${req.body.model}', '${req.body.year}', '${req.params.id}');`)
+  .then(res.send("OK"))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).send();
+  })
+})
+
+
+
 
 module.exports = router;
