ea3ddab97e1bb647125ba52d4561dc4678ac9f0f
Rye-Ton <tcriddle@smu.edu>
2019-04-30 00:21:37 -0500

    cars

--- myapp/routes/cars.js
+++ myapp/routes/cars.js
@@ -82,11 +82,12 @@ router.get('/:id', upload.array(), function(req, res, next) {
       SELECT service_id, service_type, service_desc, date
       FROM service_history_table
       WHERE car_id = ${carRow.car_id}
-      `).then(serviceRows => {
+      `).then((serviceRows, err) => {
+        if(err) throw err;
         carRow['services'] = serviceRows;
-      }).catch(err =>{
+      })
+      .catch(err =>{
         console.log(err);
-        throw err;
       })
       return carRow;
     })
