4efd267133b3a5053664c1c6de761ecf5547615a
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-05-01 00:51:26 -0500

    Updating manageAccount

--- myapp/routes/manageAccount.js
+++ myapp/routes/manageAccount.js
@@ -14,6 +14,7 @@ var database = new Database();
 
 // Manage Account Router
 router.post('/:user_id', upload.array(), function(req, res, next) {
+  console.log(body);
   database.query(
     `SELECT * FROM user_table WHERE user_id = '${req.params.user_id}';`
   ).then((rows, err) => {
